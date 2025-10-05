182285fc0b1c090bfa554ca03e9048fa950c8fe5
deepdeb <debnath.pradeep619@gmail.com>
2025-10-05 22:47:34 +0530

    Till made with heart message in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+        <p className='text-center text-[#495057] text-sm px-4 md:px-12 py-4 md:py-6'>Made with ❤️ by Pradeep</p>
+    </footer>
   )
 }
 
